2abb344053be2555a0eac0ffc0efb8a570eae6fb
Charles Thornhill-Cole <charlestc@gmail.com>
2025-06-05 07:30:07 +1000

    wip: login command

---
+++ commands.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+type command struct {
+	name string
+	args []string
+}
+
+type commands struct {
+	handlers map[string]func(*state, command) error
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	callback, found := c.handlers[cmd.name]
+	if !found {
+		return fmt.Errorf("command not found: %s", cmd.name)
+	}
+
+	err := callback(s, cmd)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+	c.handlers[name] = f
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.args) != 1 {
+		return fmt.Errorf("expected username, got %v", cmd.args)
+	}
+
+	userName := cmd.args[0]
+	if err := s.cfg.SetUser(userName); err != nil {
+		return err
+	}
+	fmt.Printf("User set to: %s", userName)
+
+	return nil
+}
--- main.go
+++ main.go
@@ -3,9 +3,15 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
+	"path"
 
 	"github.com/chuckatc/gator/internal/config"
 )
+
+type state struct {
+	cfg *config.Config
+}
 
 func main() {
 	cfg, err := config.Read()
@@ -13,14 +19,28 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	if err := cfg.SetUser("cat"); err != nil {
-		log.Fatalln(err)
+	s := state{
+		cfg: &cfg,
 	}
 
-	cfg, err = config.Read()
-	if err != nil {
-		log.Fatalln(err)
+	cmds := commands{
+		handlers: make(map[string]func(*state, command) error),
 	}
+	cmds.register("login", handlerLogin)
+
+	if len(os.Args) < 2 {
+		log.Fatalf("Usage: %s <command> [args ...]", path.Base(os.Args[0]))
+	}
+	cmdName := os.Args[1]
+	args := []string{}
+	if len(os.Args) > 2 {
+		args = os.Args[2:]
+	}
+
+	cmds.run(&s, command{name: cmdName, args: args})
+
+	fmt.Println("", cmdName, args)
+	fmt.Println("", s, cmds)
 
 	fmt.Println(cfg.CurrentUserName)
 	fmt.Println(cfg.DbURL)
